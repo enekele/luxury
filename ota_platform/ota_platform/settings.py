5c9f7e05f5bbf6d274b36fd7e8484c714474eb47
enekele <noahusman22@gmail.com>
2026-08-11 23:14:10 +0100

    Update ALLOWED_HOSTS to use environment variable

--- ota_platform/ota_platform/settings.py
+++ ota_platform/ota_platform/settings.py
@@ -10,7 +10,10 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-your-secret-key-here'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS=.onrender.com
+ALLOWED_HOSTS = os.getenv(
+    'ALLOWED_HOSTS',
+    'localhost,127.0.0.1'
+).split(',')
 
 
 CSRF_TRUSTED_ORIGINS=https://*.onrender.com
